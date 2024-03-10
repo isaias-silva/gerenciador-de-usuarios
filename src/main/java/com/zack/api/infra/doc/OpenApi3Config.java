07dbf36c1a0ddd02eda324d419464260bb0f29c5
isaias-silva <zackblackmangold@gmail.com>
2024-03-10 13:11:21 -0300

    :memo:  doc: documentação com swagger

---
+++ src/main/java/com/zack/api/infra/doc/OpenApi3Config.java
@@ -0,0 +1,20 @@
+package com.zack.api.infra.doc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "MediaCodecApi", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "api de rede social fictícia"
+)
+public class OpenApi3Config {
+
+}
