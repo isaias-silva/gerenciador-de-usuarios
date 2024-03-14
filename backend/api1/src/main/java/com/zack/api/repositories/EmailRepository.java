13e24f08f8fbb538039f67b961819d69e9315542
isaias-silva <zackblackmangold@gmail.com>
2024-03-14 17:43:16 -0300

    :rocket: deploy: conteinerização do backend

--- backend/api1/src/main/java/com/zack/api/repositories/EmailRepository.java
+++ backend/api1/src/main/java/com/zack/api/repositories/EmailRepository.java
@@ -5,11 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.ArrayList;
 import java.util.UUID;
 
+
+@Repository
+@EnableJpaRepositories
 public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 
     @Query("SELECT e.id, e.toMail,e.fromMail, e.subject FROM EmailModel e")
--- backend/api1/src/main/java/com/zack/api/repositories/UserRepository.java
+++ backend/api1/src/main/java/com/zack/api/repositories/UserRepository.java
@@ -3,12 +3,15 @@ package com.zack.api.repositories;
 import com.zack.api.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
+@EnableJpaRepositories
+
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
 
     @Query("SELECT u FROM UserModel u WHERE u.name = :name OR u.mail = :mail")
